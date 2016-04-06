a528f70967475f8f61233681f0105ffa8875ff4f
Daniel Cortes <daniel.cortes@rivetlogic.com>
2016-04-06 16:44:12 -0600

    Added basic tests for MainActivity.

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -19,15 +19,15 @@ public class MainActivity extends Activity {
     }
 
     @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.activity_main, menu);
-    return true;
-  }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-      greetingView = (TextView) findViewById(R.id.greeting);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        greetingView = (TextView) findViewById(R.id.greeting);
+    }
 }
